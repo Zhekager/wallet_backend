616fad558a209a7691aaa277aead6d7145c4147c
Tetiana1386 <tanyashostak13@gmail.com>
2021-11-14 02:59:41 +0200

    fix trans_model

--- model/transaction.js
+++ model/transaction.js
@@ -3,32 +3,51 @@ const mongoosePaginate = require('mongoose-paginate-v2');
 
 const transactionSchema = new Schema(
     {
+        transType: {
+            type: String,
+            enum: ['spend', 'income'],
+            default: 'spend',
+            required: true,
+        },
+
         date: {
             type: Date,
             min: '2020-01-01',
             required: true,
-        },
+            },
+        
         month: {
             type: Number,
             required: true,
         },
+
         year: {
             type: Number,
             required: true,
         },
+
         sum: {
             type: Number,
             required: [true, 'Укажите сумму транзакции'],
         },
+
         balance: {
             type: Number,
             required: true,
-        },
+            },
+        
+        comment: {
+            type: String,
+            maxlength: 250,
+            default: null,
+            },
+        
         categoryId: {
             type: Schema.Types.ObjectId,
             ref: 'categories',
             required: true,
         },
+        
         owner: {
             type: Schema.Types.ObjectId,
             ref: 'user',
