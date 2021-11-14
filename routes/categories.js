293403f36ea6d12a4b28568fb0d045caa402e56b
Tetiana1386 <tanyashostak13@gmail.com>
2021-11-14 02:36:21 +0200

    adds transitions, categories

---
+++ routes/categories.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const guard = require('../helpers/guard');
+const { getCategories } = require('../controllers/categories');
+
+router.get('/', guard, getCategories);
+
+module.exports = router;
