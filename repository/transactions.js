901443dbb6391e52fc7fe5a48d192c74216442d0
Tetiana1386 <tanyashostak13@gmail.com>
2021-11-19 16:17:26 +0200

    fix transactions

--- repository/transactions.js
+++ repository/transactions.js
@@ -4,7 +4,7 @@ const listTransactions = async (userId, query) => {
     const {
         sortBy = 'date',
         sortByDesc,
-        limit = 5,
+        limit = 10,
         page = 1,
         filter,
     } = query;
@@ -17,27 +17,11 @@ const listTransactions = async (userId, query) => {
             ...(sortByDesc ? { [`${sortByDesc}`]: -1 } : {}),
         },
         select: filter ? filter.split('|').join(' ') : '',
-        populate: {
-            path: 'category',
-            select: 'color',
-        },
     });
-    const { docs: transactions, ...data} = results;
-    //delete results.docs;
+    const { docs: transactions, ...data } = results;
+    delete results.docs;
     return { data, transactions };
 };
-
-// const listTransactions = async (userId) => {
-//     const results = await Transaction.find({
-//         owner: userId,
-//     })
-//         .populate({
-//             path: 'owner',
-//             select: '_id',
-//         })
-//         .sort({ date: -1 })
-//     return results
-// }
 
 const getTransactionById = async (userId, transId) => {
     const result = await Transaction.findOne({ _id: transId, owner: userId, })
@@ -66,20 +50,105 @@ const updateTransaction = async (transId, body, userId) => {
     return result;
 };
 
-const listTransStats = async (userId, month, year) => {
-    const allTransactions = await Transaction.find({ month, year });
+const listTransactionStats = async (userId, month, year) => {
+    const allTransactions = await Transaction.find({
+        owner: userId,
+        month: month,
+        year: year,
+    });
 
-    const stats = allTransactions.reduce((acc, { category, sum }) => {
-        const id = category.toString();
+    const statistic = allTransactions.reduce((acc, { isExpense, category, sum }) => ({
+        ...acc,
+        [category]:
+            acc[category] && isExpense === false
+                ? acc[category] + sum
+                : acc[category] && isExpense === true
+                    ? acc[category] - sum
+                    : !acc[category] && isExpense === false
+                        ? -sum
+                        : sum,
+    }),
+        {}
+    );
+    const totalIncome = transactions.reduce(
+        (acc, { sum, isExpense }) => (isExpense === false ? acc + sum : acc),
+        0
+    );
+    const totalExpence = transactions.reduce(
+        (acc, { sum, isExpense }) => (isExpense === true ? acc + sum : acc),
+        0
+    );
 
-        return {
-            ...acc,
-            [id]: acc[id] ? acc[id] + sum : sum,
-        };
-    }, {});
-
-    return stats;
+    const result = {
+        ...statistic,
+        totalIncome: totalIncome,
+        totalExpence: totalExpence,
+    };
+    return result;
 };
+
+// const listTransactionStats = async (month, year, userId) => {
+//     const transactions = await Transaction.find({
+//         owner: userId,
+//         month,
+//         year,
+//     })
+//         .populate({
+//             path: 'owner',
+//             select: '_id',
+//         })
+//     const income = calculateIncome(transactions)
+//     const consumption = calculateConsumption(transactions)
+
+//     const consumptionTransactions = transactions.filter((el) => !el.isExpense)
+//     const groupedTransactions = groupBy(consumptionTransactions, 'category')
+
+//     const categories = Object.keys(groupedTransactions).map((element) => {
+//         return {
+//             category: element,
+//             sum: calculateSumByCategory(groupedTransactions[element]),
+//         }
+//     })
+//     return {
+//         categories: categories,
+//         income: income,
+//         consumption: consumption,
+//         owner: userId,
+//     }
+// };
+
+// const groupBy = function (xs, key) {
+//     return xs.reduce(function (rv, x) {
+//         ; (rv[x[key]] = rv[x[key]] || []).push(x)
+//         return rv
+//     }, {})
+// }
+
+// const calculateSumByCategory = (transactions) => {
+//     return transactions.reduce(
+//         (total, element) => (total += parseInt(element.sum)),
+//         0
+//     )
+// }
+
+// const calculateIncome = (transactions) => {
+//     const incomeArray = transactions.filter((el) => el.isExpense)
+
+//     const income = incomeArray.reduce(
+//         (total, el) => (total += parseInt(el.sum)),
+//         0
+//     )
+//     return income
+// };
+
+// const calculateConsumption = (transactions) => {
+//     const consumptionArray = transactions.filter((el) => !el.isExpense)
+//     const consumption = consumptionArray.reduce(
+//         (total, el) => (total += el.sum),
+//         0
+//     )
+//     return consumption
+// };
 
 module.exports = {
     listTransactions,
@@ -87,5 +156,5 @@ module.exports = {
     removeTransaction,
     addTransaction,
     updateTransaction,
-    listTransStats,
+    listTransactionStats,
 };
