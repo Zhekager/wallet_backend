b967b121f893c9ee2c5840923a6655943c295c7b
Yevhenii Herasymenko <74263798+Zhekager@users.noreply.github.com>
2021-11-19 09:05:42 +0200

    test

--- bin/server.js
+++ bin/server.js
@@ -3,15 +3,15 @@ const app = require("../app");
 
 require("dotenv").config();
 const UPLOAD_DIR = process.env.UPLOAD_DIR;
-const AVATAR_OF_USERS = process.env.AVATAR_OF_USERS;
+//const AVATAR_OF_USERS = process.env.AVATAR_OF_USERS;
 const mkdirp = require("mkdirp");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 db.then(() => {
   app.listen(PORT, async () => {
     await mkdirp(UPLOAD_DIR);
-    await mkdirp(AVATAR_OF_USERS);
+    //await mkdirp(AVATAR_OF_USERS)
     console.log(`Server is running. Use our API on port: ${PORT}`);
   });
 }).catch((err) => {
