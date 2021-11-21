ddfa7b92d28db42fa216508f00a4482db1fe1354
Tetiana1386 <tanyashostak13@gmail.com>
2021-11-21 11:42:45 +0200

    fix conflicts

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,45 +1,26 @@
 const Categories = require("../repository/categories");
 const { HttpCode } = require("../helpers/constants");
 
-const getCategories = async (req, res) => {
-  
-=================================================================================
-  
-    const userId = req.user?._id;
-    const result = await Categories.getCategories(userId);
+const getCategories = async (_, res) => {
+    const result = await Categories.getCategories();
     return res.json({
         status: 'Success',
         code: HttpCode.OK,
         message: 'Categories found',
         result
     });
-
-  ///////////////////////////////////////////////////////////////////////////
-  
-  // const userId = req.user?._id;                          <=конфлікт
-//   const userId = req.user && req.user._id;
-
-//   const data = await Categories.listCategories(userId, req.query);
-//   res.json({
-//     status: "Success",
-//     code: HttpCode.OK,
-//     message: "Categories found",
-//     data: data,
-//   });
- ==================================================================================
-
 };
 
 const addCategories = async ({ body }, res) => {
     const result = await Categories.addCategories(body);
 
-    return res.json({
+    return res.status(201).json({
         status: 'Created',
-        code: HttpCode.CREATED,
+        code: 201,
         data: {
             result,
         },
     });
 };
 
-module.exports = { getCategories, addCategories };
+module.exports = { getCategories, addCategories }
--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,183 +1,34 @@
 const Transactions = require("../repository/transactions");
 const User = require("../repository/users");
 const { HttpCode } = require("../helpers/constants");
-===============================================================================================================
 
-const addTransactions = async ({ body, user: { _id, balance } }, res) => {
+
+const addTransactions = async ({ body, user: { id, balance } }, res) => {
   if (body.type === 'income') {
     const updatedBalance = (balance += body.money);
-    await User.updateUserBalance(_id, { balance: updatedBalance });
+    await User.updateUserBalance(id, { balance: updatedBalance });
   }
   if (body.type === 'spend') {
     const updatedBalance = (balance -= body.money);
-    await User.updateUserBalance(_id, { balance: updatedBalance });
-    
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// const getTransactions = async (req, res) => {                                             <=конфлікт
-//   // const userId = req.user?._id;
-
-//   const userId = req.user && req.user._id;
-
-//   const { data, transactions } = await Transactions.listTransactions(
-//     userId,
-//     req.query
-//   );
-//   const years = [...new Set(transactions.map(({ year }) => year))].sort();
-//   const monthes = [...new Set(transactions.map(({ month }) => month))].sort();
-//   res.json({
-//     status: "Success",
-//     code: HttpCode.OK,
-//     message: "Transactions found",
-//     data: { monthes, years, transactions, data },
-//   });
-// };
-
-// const getTransactionById = async (req, res) => {
-//   // const userId = req.user?._id;
-//   const userId = req.user && req.user._id;
-//   const transaction = await Transactions.getTransactionById(
-//     userId,
-//     req.params.transId
-//   );
-//   if (transaction) {
-//     return res.json({
-//       status: "Success",
-//       code: HttpCode.OK,
-//       message: "Transaction found",
-//       data: {
-//         transaction,
-//       },
-//     });
-//   }
-//   throw new CustomError(HttpCode.NOT_FOUND, "Not Found");
-// };
-
-// // const addTransaction = async (req, res, next) => {
-// //   try {
-// //     const userId = req.user?._id;
-// //     const balanceUser = Number(req.user?.balance);
-// //     const { sum, isExpense } = req.body;
-// //     const sumNumber = parseInt(sum);
-// //     const transactionBalance = countBalance(isExpense, balanceUser, sumNumber);
-
-// //     await User.addBalance(userId, transactionBalance);
-
-// //     const transaction = await Transactions.addTransaction({
-// //       ...req.body,
-// //       owner: userId,
-// //       balance: transactionBalance,
-// //     });
-// //     res.status(HttpCode.CREATED).json({
-// //       status: "Success",
-// //       code: HttpCode.CREATED,
-// //       data: { transaction },
-// //     });
-// //   } catch (error) {
-// //     next(error);
-// //   }
-// // };
-
-// const addTransaction = async (req, res, next) => {
-//   try {
-//     // const userId = req.user?._id;
-//     const userId = req.user && req.user._id;
-//     let transactionBalance;
-
-//     req.body.isExpense === true
-//       ? // ? (transactionBalance = Number(req.user?.balance) - Number(req.body.sum))
-//         // : (transactionBalance = Number(req.user?.balance) + Number(req.body.sum));
-//         (transactionBalance =
-//           Number(req.user && req.user.balance) - Number(req.body.sum))
-//       : (transactionBalance =
-//           Number(req.user && req.user.balance) + Number(req.body.sum));
-
-//     const newTransactionBalance = await User.addBalance(
-//       userId,
-//       transactionBalance
-//     );
-//     const transaction = await Transactions.addTransaction({
-//       ...req.body,
-//       owner: userId,
-//       balance: transactionBalance,
-//     });
-//     res.json({
-//       status: "Success",
-//       code: HttpCode.OK,
-//       message: "Successfull",
-//       data: {
-//         transaction,
-//       },
-//     });
-//     next(newTransactionBalance);
-//   } catch (err) {
-//     next(err);
-    
-==================================================================================================
-
+    await User.updateUserBalance(id, { balance: updatedBalance });
   }
 
-
-  ================================================================================================
-  
-  const result = await Transactions.addTransactions(_id, body, balance);
+  const result = await Transactions.addTransactions(id, body, balance);
 
   return res.status(HttpCode.CREATED).json({
-    
-    //////////////////////////////////////////////////////////////////////////////////////////////////
-
-// const removeTransaction = async (req, res) => {                          <=конфлікт
-//   // const userId = req.user?._id;
-//   const userId = req.user && req.user._id;
-//   const transaction = await Transactions.removeTransaction(
-//     userId,
-//     req.params.transId
-//   );
-//   if (transaction) {
-//     return res.json({
-//       status: "Success",
-//       code: HttpCode.OK,
-//       message: "Transaction deleted",
-//       data: {
-//         transaction,
-//       },
-//     });
-//   }
-//   throw new CustomError(HttpCode.NOT_FOUND, "Not Found");
-// };
-
-// const updateTransaction = async (req, res) => {
-//   const { _id: userId } = req.user;
-//   const { sum, type } = req.body;
-//   const sumNumber = parseInt(sum);
-//   // const balance = Number(req.user?.balance);
-//   const balance = Number(req.user && req.user.balance);
-
-//   const transactionBalance = countBalance(type, balance, sumNumber);
-
-//   await User.addBalance(userId, transactionBalance);
-//   const transaction = await Transactions.updateTransaction(
-//     req.params.transId,
-//     req.body,
-//     userId,
-//     transactionBalance
-//   );
-//   res.json({
-    
-    ========================================================================================
-
     status: "Success",
     code: HttpCode.CREATED,
     data: { result },
   });
 };
 
+
 const getTransactions = async ({ user: { id } }, res) => {
   const result = await Transactions.getTransactions(id);
 
   return res.json({
     status: 'Success',
-    code: HttpCode.OK,
+    code: 200,
     data: {
       total: result.length,
       result,
@@ -241,7 +92,7 @@ const getStatistics = async ({ user: { id }, query }, res) => {
 
   return res.json({
     status: 'Success',
-    code: HttpCode.OK,
+    code: 200,
     data: {
       categoriesSummary,
       totalIncome,
