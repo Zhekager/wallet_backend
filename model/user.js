00b78cfef146eb424b96202f0545e20e6a81807a
Tetiana1386 <tanyashostak13@gmail.com>
2021-11-22 22:37:05 +0200

    adds ballance/user

--- model/user.js
+++ model/user.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, SchemaTypes, model } = require("mongoose");
 
 const gravatar = require("gravatar");
 const { v4: uuidv4 } = require("uuid");
